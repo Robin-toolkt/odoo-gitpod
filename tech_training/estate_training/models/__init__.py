a71d310c0c98ec901a4bd2b6537a10ab7b980bc6
Robin-toolkt <robin.toolkt@gmail.com>
2024-08-16 09:37:30 +0000

    robin tech training

--- tech_training/estate_training/models/__init__.py
+++ tech_training/estate_training/models/__init__.py
@@ -1,3 +1,4 @@
 from . import property
 from . import property_type
 from . import property_tags
+from . import property_offer
--- tech_training/estate_training/models/property_offer.py
+++ tech_training/estate_training/models/property_offer.py
@@ -1,11 +1,12 @@
 from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'estate.property.offer'
 
     price = fields.Float("Price", required=True)
-    validity = fields.Integer("Validity (7 days)", default=7)
+    validity = fields.Integer("Validity (days)", default=7)
 
     state = fields.Selection(
         selection=[
@@ -23,4 +24,16 @@ class EstatePropertyOffer(models.Model):
         "estate.property_type", string="Property Type", store=True
     )
 
-    date_deadline = fields.Date(string="Deadline")
+    date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.date_deadline = date + relativedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.validity = (offer.date_deadline - date).days
+ 
